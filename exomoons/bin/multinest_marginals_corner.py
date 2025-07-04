12f865dcecaacd9513aa93615e1a1de3557b8d0b
Emma Yu <emmayu@login05.chn.perlmutter.nersc.gov>
2025-07-03 19:36:22 -0700

    Added initial multinest tests

---
+++ exomoons/bin/multinest_marginals_corner.py
@@ -0,0 +1,4 @@
+#!/global/u2/e/emmayu/Exomoons2025/exomoons/bin/python
+# EASY-INSTALL-SCRIPT: 'pymultinest==2.12','multinest_marginals_corner.py'
+__requires__ = 'pymultinest==2.12'
+__import__('pkg_resources').run_script('pymultinest==2.12', 'multinest_marginals_corner.py')
